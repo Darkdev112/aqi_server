c42a258dbd6941b642a4e32962c34d74831675b7
Dev Ashrit Behera <devashritcode@gmail.com>
2024-06-15 09:24:25 +0530

    cors middleware added

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import date
 from coordinates import get_coordinates
 from training.load_index import calc_index
 from model import model_predict
 
 app = FastAPI()
+
+origins=[
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
